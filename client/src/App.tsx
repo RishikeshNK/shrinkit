9fbd1524245ac906d93f7211130ad3061aedc1fb
Rishikesh Kanabar <63854326+RishikeshNK@users.noreply.github.com>
2023-08-23 09:51:46 -0400

    feat(client): setup client skeleton

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Heading, Flex } from "@chakra-ui/react";
+
+export default function App() {
+  return (
+    <>
+      <Flex height="100vh" alignItems="center" justifyContent="center">
+        <Heading>ShrinkIt</Heading>
+      </Flex>
+    </>
+  );
+}
