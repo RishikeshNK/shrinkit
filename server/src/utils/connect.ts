1cb133eac5504c2c3af8d66f2730cdacf9895c48
Rishikesh <rishikesh.kanabar@gmail.com>
2023-10-02 11:55:44 -0400

    fix: update mongo connection uri

--- server/src/utils/connect.ts
+++ server/src/utils/connect.ts
@@ -3,6 +3,7 @@ import config from "../config";
 
 export default async function connect() {
   try {
+    console.log("Trying to connect to MongoDB")
     await mongoose.connect(config.MONGO_URI!);
     console.log("Connected to MongoDB");
   } catch (e) {
